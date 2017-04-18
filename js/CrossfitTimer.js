01f881a4dd2775d043abe3c349315902d42b60a8
Mark Temple <mtemps@gmail.com>
2017-04-17 21:20:12 -0400

    Breaking tabnav into its own component

--- js/CrossfitTimer.js
+++ js/CrossfitTimer.js
@@ -4,7 +4,7 @@
  *
  * */
 var React = require('React');
-// import { Component } from 'react';
+import TabNav from './TabNav';
 
 import {
     StyleSheet,
@@ -15,73 +15,26 @@ import {
     View
 } from 'react-native';
 
+
+
 var CrossfitTimer = React.createClass({
     render() {
         return (
-                <TabBarIOS unselectedTintColor="yellow"
-                           tintColor="white"
-                           unselectedItemTintColor="red"
-                           barTintColor="darkslateblue">
-                    <TabBarIOS.Item
-                        title="Blue Tab"
-                        systemIcon="most-viewed"
-                        selected={false}>
-                        <View style={[styles.tabContent]}>
-                            <Text style={styles.tabText}>tab1</Text>
-                            <Text style={styles.tabText}>test</Text>
-                        </View>
-                    </TabBarIOS.Item>
-
-                    <TabBarIOS.Item
-                        systemIcon="history"
-                        badgeColor="black"
-                        title="Black"
-                        selected={false}>
-                        <View style={[styles.tabContent]}>
-                            <Text style={styles.tabText}>tab1</Text>
-                            <Text style={styles.tabText}>test</Text>
-                        </View>
-                    </TabBarIOS.Item>
-
-                    <TabBarIOS.Item
-                        systemIcon="recents"
-                        renderAsOriginal
-                        title="More"
-                        selected={false}>
-                        <View style={[styles.tabContent]}>
-                            <Text style={styles.tabText}>tab1</Text>
-                            <Text style={styles.tabText}>test</Text>
-                        </View>
-                    </TabBarIOS.Item>
-                </TabBarIOS>
+            <View style={[styles.container]}>
+                <Text>
+                    Welcome to the awesomeness Crossfit Timer
+                </Text>
+                <TabNav />
+            </View>
         );
     }
 });
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        flexDirection: 'column',
+        height: '100%',
         backgroundColor: '#5848FF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-    tabContent: {
-        flex: 1,
-        alignItems: 'center',
-    },
-    tabText: {
-        color: 'white',
-        margin: 50,
     },
 });
 
---
+++ js/TabNav.js
@@ -0,0 +1,88 @@
+/**
+ *
+ *
+ *
+ * */
+var React = require('React');
+
+import {
+    StyleSheet,
+    TabBarIOS,
+    Text,
+    TextInput,
+    Slider,
+    View
+} from 'react-native';
+
+var TabNav = React.createClass({
+    render() {
+        return (
+                <TabBarIOS unselectedTintColor="yellow"
+                           tintColor="white"
+                           unselectedItemTintColor="red"
+                           barTintColor="darkslateblue">
+                    <TabBarIOS.Item
+                        title="Blue Tab"
+                        systemIcon="most-viewed"
+                        selected={false}>
+                        <View style={[styles.tabContent]}>
+                            <Text style={styles.tabText}>tab1</Text>
+                            <Text style={styles.tabText}>test</Text>
+                        </View>
+                    </TabBarIOS.Item>
+
+                    <TabBarIOS.Item
+                        systemIcon="history"
+                        badgeColor="black"
+                        title="Black"
+                        selected={false}>
+                        <View style={[styles.tabContent]}>
+                            <Text style={styles.tabText}>tab1</Text>
+                            <Text style={styles.tabText}>test</Text>
+                        </View>
+                    </TabBarIOS.Item>
+
+                    <TabBarIOS.Item
+                        systemIcon="recents"
+                        renderAsOriginal
+                        title="More"
+                        selected={false}>
+                        <View style={[styles.tabContent]}>
+                            <Text style={styles.tabText}>tab1</Text>
+                            <Text style={styles.tabText}>test</Text>
+                        </View>
+                    </TabBarIOS.Item>
+                </TabBarIOS>
+        );
+    }
+});
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#5848FF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    tabContent: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    tabText: {
+        color: 'white',
+        margin: 50,
+    },
+});
+
+module.exports = TabNav;
+
